40857ee50cca85c170ce1bccbd14d9252561343b
kali <kali@kali.kali>
2021-07-12 17:59:14 +0300

    Refactor code to correspond to the PEP8 convention

--- brick_lang.py
+++ brick_lang.py
@@ -1,12 +1,12 @@
-"""This program takes a sentence and converts it to the brick language""" 
+"""This program takes a sentence and converts it to the brick language"""
 
 sentence = input("Пожалуйста, введите предложение:\n")
 
 vowel_letters = ['а', 'А', 'о', 'О', 'е', 'Е', 'ы', 'Ы', 'и', 'И', 'у', 'У']
-mod_sentence = ""
+MOD_SENTENCE = ""
 
 for v_letter in vowel_letters:
-  mod_sentence = sentence.replace(v_letter, v_letter + "к" + v_letter.lower())
-  sentence = mod_sentence
+    MOD_SENTENCE = sentence.replace(v_letter, v_letter + "к" + v_letter.lower())
+    sentence = MOD_SENTENCE
 
 print("На кирпичном языке это будет: \n" + sentence)
